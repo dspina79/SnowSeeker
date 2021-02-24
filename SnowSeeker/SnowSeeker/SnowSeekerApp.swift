bef54b38910140b2c0d31cc3ef8873efe8b42914
Dave Spina <dspina79@gmail.com>
2021-02-24 15:15:23 -0500

    Project initialization

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Dave Spina on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
